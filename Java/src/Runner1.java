b0f4a3954fcb8d6a42d02c6f5efc720d95e213c4
BroNardo <skyer4@gmail.com>
2020-06-13 15:59:37 +1000

    solved 231 power of two

---
+++ Java/src/Runner1.java
@@ -0,0 +1,19 @@
+import com.PowerOfTwo231.*;
+
+
+public class Runner1 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+
+        System.out.println(s.isPowerOfTwo(2));
+        System.out.println(2&(2-1));
+        System.out.println(4&3);
+        System.out.println(5&4);
+        System.out.println(8&7);
+        System.out.println(1&6);
+        System.out.println(1&7);
+        System.out.println(1&8);
+
+    }
+}
