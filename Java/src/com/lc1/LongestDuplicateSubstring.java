6787980673fdb727b5215f682b35cb1976ca3368
BroNardo <skyer4@gmail.com>
2020-06-21 22:17:37 +1000

    174	Dungeon Game

--- Java/src/com/lc1/LongestDuplicateSubstring.java
+++ Java/src/com/lc1/LongestDuplicateSubstring.java
@@ -1,14 +1,49 @@
 package com.lc1;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LongestDuplicateSubstring {
+    long mod = Long.MAX_VALUE/26;
     public String longestDupSubstring(String S) {
         int n = S.length();
         if(n<2) return S;
         //from 1 binary search up for a possible solution;
-        return binarySearch(S,0,n-2);
+        int left = 0;
+        int right = n-1;
+        String result="";
+        while(right>=left){
+            int mid = (left+right)/2;
+            Set<Long> set = new HashSet<>();
+            long hash = 0,power = 1;
+            boolean found = false;
+            for(int i=0;i<mid;i++){
+                hash = (26 * hash + (S.charAt(i) - 'a'))%mod;
+                power = (power * 26) % mod;
+            }
+            set.add(hash);
+
+            for(int i = 0; i + mid < n; i++){
+                hash = (hash*26 + (S.charAt(i + mid) - 'a') - ((S.charAt(i) - 'a')*power))%mod;
+                if(hash < 0) hash += mod;
+                if(set.contains(hash)){
+                    found=true;
+                    result = S.substring(i + 1, i + mid + 1);
+                    break;
+                }
+
+                set.add(hash);
+            }
+
+            if(found){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return result;
     }
 
     public String binarySearch(String S, int left, int right){
@@ -57,5 +92,58 @@ public class LongestDuplicateSubstring {
             else map.put(hash,1);
         }
         return "";
+    }
+    //below is a trie node solution
+
+    private String S;
+
+    // T = O(N*K), S = O(N), where N is length of S and K avg depth of trie.
+    public String longestDupSubstring2(String S) {
+        this.S = S;
+        int maxLo = 0, maxLength = 0;
+        TrieNode root = new TrieNode(0, 0);
+        for (int i = 1; i + maxLength < S.length(); i++) {
+            int len = addNew(root, i);
+            if (len > maxLength) {
+                maxLength = len;
+                maxLo = i;
+            }
+        }
+        return S.substring(maxLo, maxLo + maxLength);
+    }
+
+    private boolean isLeaf(TrieNode node) {
+        return node.next == null;
+    }
+
+    private int getIndex(int i, int depth) {
+        return S.charAt(i + depth) - 'a';
+    }
+
+    private int addNew(TrieNode node, int i) {
+        int depth = node.depth;
+        if (i + depth == S.length()) return depth;
+        if (isLeaf(node)) {
+            node.next = new TrieNode[26];
+            node.next[getIndex(node.i, node.depth)] = new TrieNode(node.i, depth + 1);
+        }
+        int c = getIndex(i, node.depth);
+        TrieNode x = node.next[c];
+        if (x == null) {
+            node.next[c] = new TrieNode(i, depth + 1);
+            return depth;
+        }
+        return addNew(x, i);
+    }
+
+    private static class TrieNode {
+        private TrieNode[] next;
+        private int i;
+        private int depth;
+
+        public TrieNode(int i, int depth) {
+            this.i = i;
+            this.depth = depth;
+        }
     }
 }
---
+++ Java/src/com/lc1/ValidNumber.java
@@ -0,0 +1,9 @@
+package com.lc1;
+
+public class ValidNumber {
+    public boolean isNumber(String s) {
+        s=s.trim();
+//        s= s.replace("\\+|\\-\d+","");
+        return false;
+    }
+}
